1b584ecfce8352f79a7594c4c784e978b66dc739
Cory Levinson <cjlevinson@gmail.com>
2019-09-27 12:26:54 -0400

    first commit ... doing exercise 1.2 of cryptopals )

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use hex;
+use std::ops::BitXor;
+
+#[derive(Debug, PartialEq)]
+struct ByteArray(Vec<u8>);
+
+
 fn main() {
-    println!("Hello, world!");
+
+    let bytes1 = hex::decode("1c0111001f010100061a024b53535009181c").unwrap();
+    let bytes2 = hex::decode("686974207468652062756c6c277320657965").unwrap();
+
+    let ByteArray(result) = ByteArray(bytes1) ^ ByteArray(bytes2);
+    println!("The final result: {} ", hex::encode(result));
 }
+
+impl BitXor for ByteArray {
+    type Output = Self;
+
+    fn bitxor(self, ByteArray(rhs): Self) -> Self::Output {
+        let ByteArray(lhs) = self;
+        if lhs.len() != rhs.len() {
+            panic!("Cannot perform `^` (bitxor) on ByteArrays of different length")
+        } else {
+            let res = lhs.iter()
+                         .zip(rhs.iter())
+                         .map(|(x, y)| (x ^ y))
+                         .collect();
+            ByteArray(res)
+        }
+    }
+}
+
+
